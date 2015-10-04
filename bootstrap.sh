46a5225eaf8e6e3f8ddb64b2a59aa1222635f24b
Emily Horsman <me@emilyhorsman.com>
2015-10-04 15:37:04 -0400

    Add Tomorrow-Night vim theme to the bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,4 +3,7 @@
 [[ ! -d $HOME/.dotfiles ]] && git clone git@github.com:emilyhorsman/dotfiles.git $HOME/.dotfiles
 [[ ! -d $HOME/.dotfiles/tomorrow ]] && git clone git@github.com:emilyhorsman/tomorrow-theme.git $HOME/.dotfiles/tomorrow
 source $HOME/.dotfiles/link_dot.bash
+
+mkdir -p $HOME/.vim/colors
+link_dot_to_home "tomorrow/vim/colors/Tomorrow-Night.vim" ".vim/colors/Tomorrow-Night.vim"
 link_dot_to_home "zshrc" ".zshrc"
