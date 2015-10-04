5ca78e799f1b1a8cb8cb79cc88b7ee7cd23acfb4
Emily Horsman <me@emilyhorsman.com>
2015-10-04 15:34:15 -0400

    Clone tomorrow-theme fork and add it to gitignore

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 [[ ! -d $HOME/.dotfiles ]] && git clone git@github.com:emilyhorsman/dotfiles.git $HOME/.dotfiles
+[[ ! -d $HOME/.dotfiles/tomorrow ]] && git clone git@github.com:emilyhorsman/tomorrow-theme.git $HOME/.dotfiles/tomorrow
 source $HOME/.dotfiles/link_dot.bash
 link_dot_to_home "zshrc" ".zshrc"
