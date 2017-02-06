cba92ff3a59db8ac753a4a1c2942fa85329a2f41
Emily Horsman <me@emilyhorsman.com>
2017-02-06 11:09:28 -0500

    Add cabal in to fish_user_path

--- config.fish
+++ config.fish
@@ -1,5 +1,6 @@
 eval (python3 -m virtualfish)
 source $HOME/.asdf/asdf.fish
+set -U fish_user_paths $fish_user_paths $HOME/.cabal/bin
 set -x XDG_CONFIG_HOME $HOME/.config
 set -x NVIM_TUI_ENABLE_TRUE_COLOR 1
 set -x EDITOR nvim
