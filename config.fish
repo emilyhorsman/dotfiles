3fba4970845c91e6d6293569bd7c998ded4ccecd
Emily Horsman <me@emilyhorsman.com>
2017-07-22 12:01:13 -0700

    Remove asdf.fish

--- config.fish
+++ config.fish
@@ -1,5 +1,4 @@
 eval (python3 -m virtualfish)
-source $HOME/.asdf/asdf.fish
 set -U fish_user_paths $fish_user_paths $HOME/.cabal/bin
 set -x XDG_CONFIG_HOME $HOME/.config
 set -x NVIM_TUI_ENABLE_TRUE_COLOR 1
