7b6a77c0b1c3dd345654c7c4ee6a8ab33994d2ba
Emily Horsman <me@emilyhorsman.com>
2019-01-10 17:19:10 -0500

    Emacs additions
    * Install org-mode
    * Turn on mouse support

--- emacs.el
+++ emacs.el
@@ -1,6 +1,12 @@
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
+
+(require 'org)
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
 
 ;; A decade of vim yields a vim user forever.
 (require 'evil)
@@ -12,16 +18,17 @@
 (when (version<= "26.0.50" emacs-version)
   (global-display-line-numbers-mode))
 
+(xterm-mouse-mode 1)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (atom-dark-theme evil))))
+ '(package-selected-packages (quote (org atom-dark-theme evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(font-lock-comment-face ((t (:foreground "#6B6B6B"))))
- )
+ '(font-lock-comment-face ((t (:foreground "#6B6B6B")))))
